cdf5ea42dd74959a3293727a36c6de48d69b2188
mohammedsaif001 <mdsaif4online@gmail.com>
2022-06-23 22:30:29 +0530

    BST SEARCH & Pre,Post,In Order Traversal

--- 5_BinarySearchTree/1_BST.py
+++ 5_BinarySearchTree/1_BST.py
@@ -34,5 +34,93 @@ class BinarySearchTree:
         else:
             return
 
+    def search(self, data):
+        # If BST is Empty
+        if self.key is None:
+            print("Binary Search Tree is Empty")
+            return
 
-root = BinarySearchTree(None)
+        # If root Node is the data
+        if self.key == data:
+            print("Node is Found :)")
+            return
+
+        # If data is less than root node
+        if data < self.key:
+            # If Left Subtree is present search the node
+            if self.leftChild:
+                self.leftChild.search(data)
+            else:
+                print("Node is not present in the Tree :<")
+
+        # If data is greatar than root node
+        elif data > self.key:
+            # If Right Subtree is present search the node
+            if self.rightChild:
+                self.rightChild.search(data)
+            else:
+                print("Node is not present in the Tree :<")
+
+    def preOrderTraversal(self):
+        if self.key is None:
+            print("Binary Search Tree is Empty")
+            return
+
+        # Root -> Left SubTree -> Right SubTree
+
+       # Printing Root
+        print(self.key, end=" ")
+        # Left Subtree
+        if self.leftChild:
+            self.leftChild.preOrderTraversal()
+        # Right Subtree
+        if self.rightChild:
+            self.rightChild.preOrderTraversal()
+
+    def inOrderTraversal(self):
+        if self.key is None:
+            print("Binary Search Tree is Empty")
+            return
+
+        # Root -> Left SubTree -> Right SubTree
+
+        # Left Subtree
+        if self.leftChild:
+            self.leftChild.inOrderTraversal()
+        # Printing Root
+        print(self.key, end=" ")
+        # Right Subtree
+        if self.rightChild:
+            self.rightChild.inOrderTraversal()
+
+    def postOrderTraversal(self):
+        if self.key is None:
+            print("Binary Search Tree is Empty")
+            return
+
+        # Root -> Left SubTree -> Right SubTree
+
+        # Left Subtree
+        if self.leftChild:
+            self.leftChild.postOrderTraversal()
+        # Right Subtree
+        if self.rightChild:
+            self.rightChild.postOrderTraversal()
+        # Printing Root
+        print(self.key, end=" ")
+
+
+root = BinarySearchTree(10)
+list1 = [21, 10, 30, 5, 12, 25, 100, 3, 7]
+for data in list1:
+    root.insert(data)
+root.search(34)
+
+print("\nPre Order Traversal")
+root.preOrderTraversal()
+
+print("\nIn Order Traversal")
+root.inOrderTraversal()
+
+print("\nPost Order Traversal")
+root.postOrderTraversal()

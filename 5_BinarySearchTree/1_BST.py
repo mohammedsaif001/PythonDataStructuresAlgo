28146fc3228d3bfdc185c4f6599dbd20cbb6b08c
mohammedsaif001 <mdsaif4online@gmail.com>
2022-06-23 21:17:43 +0530

    BST Insertion

--- 5_BinarySearchTree/1_BST.py
+++ 5_BinarySearchTree/1_BST.py
@@ -4,15 +4,35 @@ class BinarySearchTree:
         self.leftChild = None
         self.rightChild = None
 
+    def insert(self, data):
+        # Checking if BST isempty where 'root' is the obkject passed to self
+        if self.key is None:
+            self.key = data
+            return
 
-root = BinarySearchTree(10)
-print(root.key)
-print(root.leftChild)
-print(root.rightChild)
+        # If Value is less than Root Node
+        if data < self.key:
+            # If lchild contains address (having subtree)
+            if self.leftChild:
+                # Call insert function again until we dont find sub-tree and find proper value for the tree
+                self.leftChild.insert(data)
+            else:
+                # Inserting Data in Left child
+                self.leftChild = BinarySearchTree(data)
 
-root.leftChild = BinarySearchTree(5)
-print(root.leftChild.key)
-print(root.leftChild.leftChild)
-print(root.leftChild.rightChild)
+        # If Value is greater than Root Node
+        elif data > self.key:
+            # If rchild contains address (having subtree)
+            if self.rightChild:
+                # Call insert function again until we dont find sub-tree and find proper value for the tree
+                self.rightChild.insert(data)
+            else:
+                # Inserting Data in Right child
+                self.rightChild = BinarySearchTree(data)
 
-print(root.leftChild)
+        # Duplicate Values is being ignored
+        else:
+            return
+
+
+root = BinarySearchTree(None)
